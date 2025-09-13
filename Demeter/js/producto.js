5179d285a83e3fb16475758d2acd73ecaa2b1c78
Martin <mar.calderona@duocuc.cl>
2025-09-12 23:53:34 -0300

    se adapto el footer y se arreglo la superposicion de elementos se establecio un diseño general para la pagina, en revision se establecio un constructor para los productos y un nuevo metodo para agregarlos, aun por finalizar

---
+++ Demeter/js/producto.js
@@ -0,0 +1,18 @@
+class producto{
+    id;
+    nombre;
+    precio;
+    cantidad;
+    total;
+    imagen;
+
+    constructor(id,nombre,precio,cantidad,total,imagen){
+        this.id=id;
+        this.nombre=nombre;
+        this.precio=precio;
+        this.cantidad=cantidad;
+        this.total=total;
+        this.imagen=imagen;
+    }   
+    
+}
