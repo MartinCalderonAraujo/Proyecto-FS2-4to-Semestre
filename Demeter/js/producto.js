1405576c0bc251df3e45c93e84182a841163adba
SebastianGarrido05 <se.garridom@duocuc.cl>
2025-09-21 23:04:18 -0300

    Solucionado los errores de producto.js / Funcionamiento completo de traspaso de datos desde el catalogo al carrito (LocalStorage)

--- Demeter/js/producto.js
+++ Demeter/js/producto.js
@@ -1,4 +1,4 @@
-class Producto{
+class Producto {
     id;
     nombre;
     precio;
@@ -6,17 +6,17 @@ class Producto{
     total;
     imagen;
 
-    constructor(id,nombre,precio,cantidad,total,imagen){
-        this.id=id;
-        this.nombre=nombre;
-        this.precio=precio;
-        this.cantidad=cantidad;
-        this.total=total;
-        this.imagen=imagen;
-    }    
+    constructor(id, nombre, precio, cantidad, total, imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.total = total;
+        this.imagen = imagen;
+    }
 }
 
-
+/*
 // TEST NO USAR
 function agregarProductooooo(id) {
     console.log("Seleccion: " + id);
@@ -31,11 +31,17 @@ function agregarProductooooo(id) {
     document.getElementById("footer2").innerHTML = valor;
     document.getElementById("footer1").innerHTML = nombreProducto;
 }
+*/
+
 // TEST NO USAR
-var productos=[
-    new Producto(1,"Humedad",30000,"img/sensor de humedad.jpg"),
-    new Producto(2,"Luz",30000,"img/sensor de luz.jpg"),
-    new Producto(3,"Voltaje",30000,"img/sensor de voltaje.jpg"),
+
+var productos = [
+    new Producto(1, "Humedad", 30000, "img/sensor de humedad.jpg"),
+    new Producto(2, "Luz", 30000, "img/sensor de luz.jpg"),
+    new Producto(3, "Voltaje", 30000, "img/sensor de voltaje.jpg"),
+    new Producto(4, "Ph", 30000, "img/sensor de ph.jpg"),
+    new Producto(5, "Temperatura", 30000, "img/sensor de temperatura.jpg"),
+    new Producto(6, "Co2", 30000, "img/sensor de co2.jpg"),
 ]
 
 // se dejara el listado en LocalStorage
@@ -44,28 +50,28 @@ var productos=[
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // se dejara el listado en LocalStorage
-localStorage.setItem("productos",JSON.stringify(productos));
+localStorage.setItem("productos", JSON.stringify(productos));
 
-function agregarProducto(id){
-    console.log("Selecciono:"+id);
-    const data=JSON.parse(localStorage.getItem("productos"))
+function agregarProducto(id) {
+    console.log("Selecciono:" + id);
+    const data = JSON.parse(localStorage.getItem("productos"))
     // recorrer el arreglo
     console.log(data[id - 1]);
     console.log(data[id - 1]["nombre"])
 
     // creacion del nuevo reg.
-    var productoCarro=new Producto()
-    productoCarro.id=data[id-1]["id"]
-    productoCarro.nombre=data[id-1]["nombre"]
-    productoCarro.precio=data[id-1]["precio"]
-    productoCarro.cantidad=1
-    productoCarro.total=data[id-1]["precio"]
-    productoCarro.imagen=data[id-1]["imagen"]
+    var productoCarro = new Producto()
+    productoCarro.id = data[id - 1]["id"]
+    productoCarro.nombre = data[id - 1]["nombre"]
+    productoCarro.precio = data[id - 1]["precio"]
+    productoCarro.cantidad = 1
+    productoCarro.total = data[id - 1]["precio"]
+    productoCarro.imagen = data[id - 1]["imagen"]
 
-    let nombre_producto=data[id-1]["nombre"];
-    
-    var valor=parseInt(document.getElementById("productos").innerHTML);
-    valor=valor+1;
+    let nombre_producto = data[id - 1]["nombre"];
+
+    var valor = parseInt(document.getElementById("productos").innerHTML);
+    valor = valor + 1;
     //console.log(valor);
     document.getElementById("productos").innerHTML = valor;
     let items = 0
@@ -76,14 +82,14 @@ function agregarProducto(id){
     var carrito = JSON.parse(localStorage.getItem("carrito"))
     if (!carrito) {
         console.log("no existe")
-        carrito=[
+        carrito = [
             productoCarro,
         ]
-        items=1
-        subtotal=productoCarro.precio
-        iva= productoCarro.precio*0.19
-        total=subtotal+iva
-        localStorage.setItem("carrito",JSON.stringify(carrito))
+        items = 1
+        subtotal = productoCarro.precio
+        iva = productoCarro.precio * 0.19
+        total = subtotal + iva
+        localStorage.setItem("carrito", JSON.stringify(carrito))
         console.log("Producto Agregado")
     } else {
         //console.log("existe")
@@ -92,7 +98,7 @@ function agregarProducto(id){
 
         carrito.forEach(item => {
             // console.log("id:"+item.id+ " nombre:"+item.nombre+ " cantidad:"+item.cantidad)
-            if(item.id==productoCarro.id){
+            if (item.id == productoCarro.id) {
                 //console.log("Existe producto en elc carro:"+item.id)
                 let cant = item.cantidad + 1
                 let total = item.precio * cant
@@ -103,7 +109,7 @@ function agregarProducto(id){
             }
             pos = pos + 1
         })
-        if(existe==0){
+        if (existe == 0) {
             carrito.push(productoCarro)
         }
         /// totales
@@ -116,9 +122,9 @@ function agregarProducto(id){
         localStorage.setItem("carrito", JSON.stringify(carrito))
         console.log("Producto Agregado")
     }
-    document.getElementById("items").innerHTML = items
-    document.getElementById("subtotal").innerHTML = subtotal
-    document.getElementById("iva").innerHTML = iva
-    document.getElementById("total").innerHTML = total
+    // document.getElementById("items").innerHTML = items
+    // document.getElementById("subtotal").innerHTML = subtotal
+    // document.getElementById("iva").innerHTML = iva
+    // document.getElementById("total").innerHTML = total
 }
 
