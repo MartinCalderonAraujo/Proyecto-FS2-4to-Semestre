5ab3ddd51bf5891e093f3af63cf556bb83794f6c
SebastianGarrido05 <se.garridom@duocuc.cl>
2025-09-21 22:20:03 -0300

    Terminacion de arreglos en producto.js

--- Demeter/js/producto.js
+++ Demeter/js/producto.js
@@ -1,4 +1,4 @@
-class producto{
+class Producto{
     id;
     nombre;
     precio;
@@ -6,7 +6,7 @@ class producto{
     total;
     imagen;
 
-    Producto(id,nombre,precio,cantidad,total,imagen){
+    constructor(id,nombre,precio,cantidad,total,imagen){
         this.id=id;
         this.nombre=nombre;
         this.precio=precio;
@@ -14,10 +14,11 @@ class producto{
         this.total=total;
         this.imagen=imagen;
     }    
-    
 }
 
-function agregarProducto(id) {
+
+// TEST NO USAR
+function agregarProductooooo(id) {
     console.log("Seleccion: " + id);
     const data = JSON.parse(localStorage.getItem("productos"))
 
@@ -30,36 +31,39 @@ function agregarProducto(id) {
     document.getElementById("footer2").innerHTML=valor;
     document.getElementById("footer1").innerHTML=nombreProducto;
 }
-
+// TEST NO USAR
 var productos=[
-            new Producto(1,"Humedad",15000,"img/sensor de humedad.jpg"),
-            new Producto(2,"Luz",1000,"img/sensor de luz.jpg"),
-            new Producto(3,"Voltaje",21000,"img/sensor de voltaje.jpg"),
-        ]
-        // se dejara el listado en LocalStorage
-        localStorage.setItem("productos",JSON.stringify(productos));
+    new Producto(1,"Humedad",30000,"img/sensor de humedad.jpg"),
+    new Producto(2,"Luz",30000,"img/sensor de luz.jpg"),
+    new Producto(3,"Voltaje",30000,"img/sensor de voltaje.jpg"),
+]
 
+// se dejara el listado en LocalStorage
+// localStorage.setItem("productos",JSON.stringify(productos));
 
-// MESCLAR CON EL OTRO AGREGAR PRODUCTO, ESTE ES UN COPY PASTE DEL PROYECTO DE LOS PASTELES
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function agregarProductoOdl(id){
+// se dejara el listado en LocalStorage
+localStorage.setItem("productos",JSON.stringify(productos));
+
+function agregarProducto(id){
     console.log("Selecciono:"+id);
-    const data=JSON.parse(localStorage.getItem("pasteles"))
-    
+    const data=JSON.parse(localStorage.getItem("productos"))
     // recorrer el arreglo
     console.log(data[id-1]);
     console.log(data[id-1]["nombre"])
 
     // creacion del nuevo reg.
-    var pastelCarro=new productoPastel()
-    pastelCarro.id=data[id-1]["id"]
-    pastelCarro.nombre=data[id-1]["nombre"]
-    pastelCarro.precio=data[id-1]["precio"]
-    pastelCarro.cantidad=1
-    pastelCarro.total=data[id-1]["precio"]
-    pastelCarro.imagen=data[id-1]["imagen"]
+    var productoCarro=new Producto()
+    productoCarro.id=data[id-1]["id"]
+    productoCarro.nombre=data[id-1]["nombre"]
+    productoCarro.precio=data[id-1]["precio"]
+    productoCarro.cantidad=1
+    productoCarro.total=data[id-1]["precio"]
+    productoCarro.imagen=data[id-1]["imagen"]
 
-    let nombre_pastel=data[id-1]["nombre"];
+    let nombre_producto=data[id-1]["nombre"];
+    
     var valor=parseInt(document.getElementById("productos").innerHTML);
     valor=valor+1;
     //console.log(valor);
@@ -73,11 +77,11 @@ function agregarProductoOdl(id){
     if(!carrito){
         console.log("no existe")
         carrito=[
-            pastelCarro,
+            productoCarro,
         ]
         items=1
-        subtotal=pastelCarro.precio
-        iva= pastelCarro.precio*0.19
+        subtotal=productoCarro.precio
+        iva= productoCarro.precio*0.19
         total=subtotal+iva
         localStorage.setItem("carrito",JSON.stringify(carrito))
         console.log("Producto Agregado")
@@ -88,7 +92,7 @@ function agregarProductoOdl(id){
         
         carrito.forEach(item =>{
             // console.log("id:"+item.id+ " nombre:"+item.nombre+ " cantidad:"+item.cantidad)
-            if(item.id==pastelCarro.id){
+            if(item.id==productoCarro.id){
                 //console.log("Existe producto en elc carro:"+item.id)
                 let cant= item.cantidad+1
                 let total=item.precio*cant 
@@ -100,7 +104,7 @@ function agregarProductoOdl(id){
             pos=pos+1                    
         })
         if(existe==0){
-            carrito.push(pastelCarro)
+            carrito.push(productoCarro)
         }
         /// totales
         carrito.forEach(i=>{
@@ -117,5 +121,4 @@ function agregarProductoOdl(id){
     document.getElementById("iva").innerHTML=iva
     document.getElementById("total").innerHTML=total
 }
-
 
